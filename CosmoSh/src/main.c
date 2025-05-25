8e3d51bb1b88cb50de93cf3cafd8412ae4b01ed8
lcello <cellittilorenzoh@gmail.com>
2025-05-25 11:26:56 +0200

    setup Tesina OS

--- CosmoSh/src/main.c
+++ CosmoSh/src/main.c
@@ -1,7 +1,34 @@
+// INLCUDES
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
+#include<sys/wait.h>
 
-int main(int argc, char** argv){
-	
-	printf("hello\n");
+//DEFINES
+#define BUFF_SZE 4096 
+
+int main(int argc, char** argv)
+{
+	pid_t pid;
+
+	while(1){
+
+		char command[BUFF_SZE];
+		fgets(command, BUFF_SZE, stdin);
+		
+		pid = fork();
+
+		if(pid != 0) {
+			if(pid == -1) {
+				printf("fork failed");
+				exit(EXIT_FAILURE);
+			}
+		} else {
+
+		}
+	}	
+
 	return 0;
 }
